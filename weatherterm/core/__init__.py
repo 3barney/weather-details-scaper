208f250e5cb55e5688b5c76465a4e40b32c3a3b5
barney <brnbsjm@gmail.com>
2019-01-08 15:45:59 +0300

    main entry file creation

--- weatherterm/core/__init__.py
+++ weatherterm/core/__init__.py
@@ -3,3 +3,5 @@ from .base_enum import BaseEnum
 from .unit_converter import UnitConverter
 from .forecast_type import ForecastType
 from .forecast import Forecast
+from .request import Request
+from .set_unit_action import SetUnitAction
